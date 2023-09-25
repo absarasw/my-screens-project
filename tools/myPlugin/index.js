6b820bd12708f1e5f98b36e7011ca9650543b368
Abhinav Saraswat <51747444+absarasw@users.noreply.github.com>
2023-09-25 23:45:50 +0530

    Update index.js

--- tools/myPlugin/index.js
+++ tools/myPlugin/index.js
@@ -1,5 +1,9 @@
 
     setTimeout(() => {
+        var url = (window.location != window.parent.location)
+            ? document.referrer
+            : document.location.href;
+    console.log('Parent URL is ' + url);    
     console.log('location is ' + window.location.href);
       alert('BUTTON CLICKED');
     }, 1000);
