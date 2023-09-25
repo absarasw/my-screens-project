cad77c7d0b99002c7a128cee63f57db776a056f8
Abhinav Saraswat <51747444+absarasw@users.noreply.github.com>
2023-09-26 00:30:11 +0530

    Update index.js

--- tools/myPlugin/index.js
+++ tools/myPlugin/index.js
@@ -1,7 +1,13 @@
 
-setTimeout(() => {  
+ previewAndRedirect = () => {
+  var params = new URLSearchParams(window.location.search);
+  var previewHost = `https://${params.get("ref")}--${params.get("repo")}--${params.get("owner")}.hlx.page`;
+  var statusUrl = `https://admin.hlx.page/status/${params.get("owner")}/${params.get("repo")}/${params.get("ref")}?editUrl=${params.get("referrer")}`;
   console.log('location is ' + window.location.href);
-}, 1000);
+  console.log("previewHost = " + previewHost);
+  console.log("statusUrl = " + statusUrl);
+}
 
+previewAndRedirect();
 
 
